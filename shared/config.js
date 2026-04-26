f362fb874671f059ebbfbcac6dd460f6303865d5
dudeaprp-cmd <dudeaprp@gmail.com>
2026-04-26 22:43:03 +0100

    Update config.js

--- shared/config.js
+++ shared/config.js
@@ -1,1 +1,7 @@
+window.APRP_CONFIG = {
+  supabaseUrl: "PASTE_YOUR_SUPABASE_PROJECT_URL_HERE",
+  supabaseAnonKey: "PASTE_YOUR_SUPABASE_PUBLISHABLE_KEY_HERE",
 
+  defaultElectionSlug: "2004-president",
+  refreshSeconds: 30
+};
